8fad24f2eacca5d801a09c65ab3c571e1ca754a4
Bruno Borghese <bb_borghese@hotmail.com>
2019-12-05 00:13:44 -0200

    monthView and sass

---
+++ front/workinghour/src/components/monthView.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react';
+
+export default class MonthView extends PureComponent {
+    render() {
+        return <div className="monthView">
+            <span>MÊS</span>
+        </div>;
+    }
+}
